f186c007af2713bf78767548920cac504b0a73a8
UPT Valencia <uptvalencia@gmail.com>
2019-02-20 02:19:03 +0000

    Actualización nombre proyecto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to web-uptval!');
+    expect(page.getTitleText()).toEqual('Welcome to uptvalencia!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-uptval'`, () => {
+  it(`should have as title 'uptvalencia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('web-uptval');
+    expect(app.title).toEqual('uptvalencia');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-uptval!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to uptvalencia!');
   });
 });
